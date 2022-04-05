1d55c139c615d278585af76426e5ba8205fe8161
39xdgy <39xdgy@gmail.com>
2022-04-04 23:57:08 -0500

    create endpoint for create default stock data. TODO for indicator and get tickers. fix typo for create stock.

--- routes/stock.py
+++ routes/stock.py
@@ -5,6 +5,8 @@ from config.db import conn
 from schemas.stock import stockEntity, stocksEntity
 from bson import ObjectId
 
+import yfinance as yf
+import stockstats
 
 stock = APIRouter()
 
@@ -22,7 +24,7 @@ async def find_one_stock_id(id):
 
 @stock.post('/')
 async def create_stock(stock: Stock):
-    conn.local.user.insert_one(dict(stock))
+    conn.local.stock.insert_one(dict(stock))
     return stocksEntity(conn.local.stock.find())
 
 @stock.put('/{id}')
@@ -35,3 +37,43 @@ async def update_stock(id, stock: Stock):
 @stock.delete('/{id}')
 async def delete_stock(id):
     return stockEntity(conn.local.stock.find_one_and_delete({"_id": ObjectId(id)}))
+
+@stock.post('/create_default_data')
+async def create_default_data():
+    all_ticker = []
+
+    for file_name in ['nasdaqlisted.txt', 'otherlisted.txt']:
+        all_data = open(f'files/{file_name}', 'r')
+        for line in all_data:
+            info = line.split("|")
+            ticker = info[0]
+            if 'File Creation Time' not in ticker and 'Symbol' not in ticker and "$" not in ticker and "." not in ticker and ticker not in all_ticker:
+                all_ticker.append(ticker)
+    print(len(all_ticker))
+
+    for ticker in all_ticker:
+        # ticker_aapl.info['sector']
+        # ticker_aapl.info['website'] 
+        ticker_obj = yf.Ticker(ticker)
+        stock_data = yf.download(ticker, period = '7d', interval = '1m')
+        # TODO would grab data from indicator database for creating all the data
+        stockStat = stockstats.StockDataFrame.retype(stock_data)
+        
+        for indicator_name in ['macd', 'kdjk', 'kdjd', 'kdjj']:
+            stock_data[indicator_name] = stockStat[[indicator_name]]
+        
+        category = 'N/A'
+        link = 'N/A'
+        if 'sector' in ticker_obj.info:
+            category = ticker_obj.info['sector']
+        if 'website' in ticker_obj.info:
+            link = ticker_obj.info['website']
+        stock_data.index = stock_data.index.astype(str)
+        conn.local.stock.insert_one({
+            'name': ticker,
+            'category': category,
+            'link': link,
+            'data': stock_data.T.to_dict()
+        })
+
+    return {"status": "Success"}

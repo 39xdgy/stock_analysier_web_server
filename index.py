e6183817d85bcbb45a612d70319115d3e7836ecc
39xdgy <39xdgy@gmail.com>
2023-01-25 23:20:43 -0600

    add cors

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI 
+from fastapi.middleware.cors import CORSMiddleware
 from routes.user import user
 from routes.strategy import strategy
 from routes.stock import stock
@@ -7,6 +8,17 @@ from routes.indicator import indicator
 import uvicorn
 
 app = FastAPI()
+
+origins = ["*"]
+#origins = ["https://localhost.example.com"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="origins",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(user, prefix='/user')
 app.include_router(strategy, prefix='/strategy')
